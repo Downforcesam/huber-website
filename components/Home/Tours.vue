35074e8c54fd0466d4ee14380415a66def79f269
Laurenz Saam <laurenz.saam@example.com>
2024-04-20 14:50:38 +0200

    Update tour links to use NuxtLink instead of anchor tags

--- components/Home/Tours.vue
+++ components/Home/Tours.vue
@@ -38,10 +38,10 @@
               >
                 <p class="flex-auto">{{ tour.description }}</p>
                 <p class="mt-6">
-                  <a
-                    :href="tour.href"
+                  <NuxtLink
+                    :to="`/${tour.slug}`"
                     class="text-sm font-semibold leading-6 text-indigo-600"
-                    >Learn more <span aria-hidden="true">→</span></a
+                    >Learn more <span aria-hidden="true">→</span></NuxtLink
                   >
                 </p>
               </dd>
@@ -55,62 +55,4 @@
 
 <script setup>
 const { locale } = useI18n();
-const tours = [
-  {
-    name: 'Cusco City Tour',
-    description:
-      'Embark on a journey through time as you explore the cobblestone streets and majestic plazas of Cusco on our City Tour. Discover architectural marvels like the imposing Cathedral and the ancient Inca fortress of Sacsayhuamán, while soaking in the vibrant atmosphere of this charming city.',
-    href: '#',
-    imageSrc: '/cusco-city.jpeg',
-    imageAlt: 'Cusco City Tour',
-  },
-  {
-    name: 'Sacred Valley',
-    description:
-      'Venture into the Sacred Valley of the Incas, where ancient ruins, bustling markets, and breathtaking scenery await. Marvel at the agricultural terraces of Pisac, wander through the ruins of Ollantaytambo, and immerse yourself in the rich culture of indigenous communities.',
-    href: '#',
-    imageSrc: '/sacred-valley.jpeg',
-    imageAlt: 'Sacred Valley Tour',
-  },
-  {
-    name: 'Machu Picchu',
-    description:
-      'No visit to Cusco is complete without a pilgrimage to the iconic Machu Picchu. Trek through lush cloud forests, traverse ancient Inca trails, and behold the awe-inspiring beauty of this UNESCO World Heritage Site. Let the magic of Machu Picchu transport you to a bygone era of Inca civilization.',
-    href: '#',
-    imageSrc: '/machu-picchu-lama.jpeg',
-    imageAlt: 'Machu Picchu Tour',
-  },
-  {
-    name: 'Maras Moray',
-    description:
-      'Discover the fascinating agricultural terraces of Maras and the enigmatic circular depressions of Moray. Learn about ancient Inca farming techniques and marvel at the stunning panoramic views of the Andean landscape.',
-    href: '#',
-    imageSrc: '/maras-moray.jpeg',
-    imageAlt: 'Maras Moray Tour',
-  },
-  {
-    name: 'Alpaca Ceremony in Patacancha',
-    description:
-      'Experience the mystical connection between Andean communities and their beloved alpacas with a traditional ceremony in Patacancha. Witness colorful rituals, learn about the importance of alpacas in Andean culture, and even participate in weaving demonstrations.',
-    href: '#',
-    imageSrc: '/quechua.jpeg',
-    imageAlt: 'Alpaca Ceremony in Patacancha Tour',
-  },
-  {
-    name: 'Rainbow Mountain (Vinicunca) and Palcoyo',
-    description:
-      'Embark on an unforgettable adventure to the breathtaking Rainbow Mountain and Palcoyo. Hike through rugged terrain, surrounded by stunning vistas of snow-capped peaks and vibrant mineral deposits, as you reach the surreal landscapes of Vinicunca and Palcoyo.',
-    href: '#',
-    imageSrc: '/rainbow-mountain.jpeg',
-    imageAlt: 'Rainbow Mountain Tour',
-  },
-  {
-    name: 'Trekking Adventures',
-    description:
-      'For the intrepid explorer, Cusco offers a plethora of trekking opportunities. From the challenging trails of Choquequirao and Salkantay to the off-the-beaten-path routes of Lares and Inca Jungle, immerse yourself in the raw beauty of the Andean wilderness.',
-    href: '#',
-    imageSrc: '/trekking.jpeg',
-    imageAlt: 'Trekking Tour',
-  },
-];
 </script>
--- pages/[slug].vue
+++ pages/[slug].vue
@@ -1,1 +1,10 @@
-<template></template>
+<template>
+  <article
+    class="min-h-screen bg-white flex flex-col items-center justify-center"
+  >
+    <h1 class="text-black text-2xl font-semibold">{{ slug }}</h1>
+  </article>
+</template>
+<script setup>
+const { slug } = useRoute().params;
+</script>
