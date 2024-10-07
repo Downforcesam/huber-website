ec595d2e995f20bde42350cb82cdf69617dc52df
Laurenz Saam <laurenz.saam@example.com>
2024-10-07 14:44:39 +0200

    refactor: Update LangSwitcher component to use cursor-pointer for language selection

--- app/components/App/LangSwitcher.vue
+++ app/components/App/LangSwitcher.vue
@@ -1,15 +1,13 @@
 <template>
-  <Menu as="div" class="relative z-50 ml-1">
-    <div>
-      <MenuButton
-        class="relative flex bg-gray-800 rounded-full focus:ring-offset-2 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-gray-800"
-      >
-        <span class="absolute -inset-1.5" />
-        <span class="sr-only">Open user menu</span>
-        <!-- <img class="rounded-full w-8 h-8" :src="`/${locale}.svg`" alt="" /> -->
-        <span class="px-4 py-2 text-sm text-white uppercase">{{ locale }}</span>
-      </MenuButton>
-    </div>
+  <Menu as="div" class="relative z-50 ml-1" v-slot="{ open }">
+    <MenuButton
+      class="relative flex bg-gray-800 rounded-full focus:ring-offset-2 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-gray-800"
+    >
+      <span class="absolute -inset-1.5" />
+      <span class="sr-only">Open user menu</span>
+      <!-- <img class="rounded-full w-8 h-8" :src="`/${locale}.svg`" alt="" /> -->
+      <span class="px-4 py-2 text-sm text-white uppercase">{{ locale }}</span>
+    </MenuButton>
 
     <MenuItems
       class="right-0 z-10 absolute bg-white ring-opacity-5 shadow-lg mt-2 py-1 rounded-md ring-1 ring-black w-12 uppercase origin-top-right focus:outline-none"
@@ -21,10 +19,11 @@
         v-slot="{ active }"
       >
         <a
-          :href="switchLocalePath(lang)"
+          @click="setLocale(lang)"
           :class="[
             active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
             'block px-4 py-2 text-sm',
+            'cursor-pointer',
           ]"
         >
           {{ lang }}
